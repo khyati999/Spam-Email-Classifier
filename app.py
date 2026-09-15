8be1e905673d7cb376bf783a08ad28c65932eab1
khyati999 <khyatimishra999>
2026-09-15 11:12:28 +0000

    Refactor prediction logic into a separate utility function and update API endpoints

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
-
+from utils import make_prediction
 app = Flask(__name__)
-cv=pickle.load(open('models/cv.pkl', 'rb'))    
-clf=pickle.load(open('models/clf.pkl', 'rb'))  
+  
 
 @app.route('/')
 def home():
@@ -12,19 +10,15 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     email = request.form.get('content')
-    tokenized_email = cv.transform([email])
-    prediction = clf.predict(tokenized_email)
-    prediction = 1 if prediction == 1 else -1
+    prediction = make_prediction(email)
     return render_template('index.html', prediction=prediction, email=email)  
     
 @app.route("/api/predict", methods=['POST'])
 def api_predict():  
     data= request.get_json(force=True)
     email= data['content']
-    tokenized_email = cv.transform([email])
-    prediction = clf.predict(tokenized_email)
-    prediction = 1 if prediction == 1 else -1
-    return jsonify({'prediction': prediction})
+    prediction = make_prediction(email)   
+    return jsonify({'prediction': prediction, 'email': email})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)             # To show updated changes
