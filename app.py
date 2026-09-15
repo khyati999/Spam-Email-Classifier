c13ca5cf009572f169f7586d866492ea44271a55
Khyati Mishra <95746392+khyati999@users.noreply.github.com>
2026-09-15 10:40:54 +0000

    Add API endpoint for email prediction with JSON response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -17,6 +17,14 @@ def predict():
     prediction = 1 if prediction == 1 else -1
     return render_template('index.html', prediction=prediction, email=email)  
     
+@app.route("/api/predict", methods=['POST'])
+def api_predict():  
+    data= request.get_json(force=True)
+    email= data['content']
+    tokenized_email = cv.transform([email])
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)             # To show updated changes
